f0cf4ec6d9045e1c05c315a8945c3ca85bd91c4f
yukiycu <i180634g@gmail.com>
2023-03-09 12:25:02 +0900

    delete unnecessary comments

--- mol_handler.py
+++ mol_handler.py
@@ -4,10 +4,8 @@ def get_HbondIdx(mol, p_index, matches):
         if bond.GetEndAtom().GetSymbol() == 'H':
             return mol.GetBondBetweenAtoms(matches[p_index],bond.GetEndAtom().GetIdx()).GetIdx()
 
-#ベースになるものとして, Glyでないくっつ
 def mol_with_atom_index( mol ):
     atoms = mol.GetNumAtoms()
     for idx in range( atoms ):
         mol.GetAtomWithIdx( idx ).SetProp( 'molAtomMapNumber', str( mol.GetAtomWithIdx( idx ).GetIdx() ) )
     return mol
-
