0cd8186c937a51aff649ca8ef8c8c79599db637c
yukiycu <i180634g@gmail.com>
2023-03-09 13:41:38 +0900

    Delete blanks

--- acquisition_function.py
+++ acquisition_function.py
@@ -5,15 +5,12 @@ import scipy.stats as stats
 def get_EI_list(train_Y, pred_y, sigma2_pred):
     prediction = pred_y
     sig = sigma2_pred**0.5
-
     gamma = (prediction - np.max(train_Y)) / sig
     ei = sig*(gamma*stats.norm.cdf(gamma) + stats.norm.pdf(gamma))
-
     return ei
 
 def calc_EI_overfmax(fmean, fcov, fmax):
     fstd = np.sqrt(fcov)
-
     temp1 = fmean - fmax
     temp2 = temp1 / fstd
     score = temp1 * stats.norm.cdf(temp2) + fstd * stats.norm.pdf(temp2)
@@ -21,14 +18,12 @@ def calc_EI_overfmax(fmean, fcov, fmax):
 
 def calc_PI_overfmax(fmean, fcov, fmax):
     fstd = np.sqrt(fcov)
-
     temp = (fmean - fmax) / fstd
     score = stats.norm.cdf(temp)
     return score
 
 def calc_PI_underfmin(fmean, fcov, fmin):
     fstd = np.sqrt(fcov)
-
     temp = (fmin - fmean) / fstd
     score = stats.norm.cdf(temp)
     return score
