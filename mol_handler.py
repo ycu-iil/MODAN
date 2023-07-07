8f615cde1c566318217f3e2e992286e54a342773
yukiycu <i180634g@gmail.com>
2023-07-07 11:51:15 +0900

    add space

--- mol_handler.py
+++ mol_handler.py
@@ -2,7 +2,7 @@
 def get_HbondIdx(mol, p_index, matches):
     for bond in mol.GetAtomWithIdx(matches[p_index]).GetBonds():
         if bond.GetEndAtom().GetSymbol() == 'H':
-            return mol.GetBondBetweenAtoms(matches[p_index],bond.GetEndAtom().GetIdx()).GetIdx()
+            return mol.GetBondBetweenAtoms(matches[p_index], bond.GetEndAtom().GetIdx()).GetIdx()
 
 def mol_with_atom_index(mol):
     atoms = mol.GetNumAtoms()
